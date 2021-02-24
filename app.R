fef3710bbacb0c8d564ba07e90d3329fa0a1f447
Okey <dynamicsoorya2691@gmail.com>
2021-02-24 16:22:20 -0500

    Added a way to change map backgrounds

--- app.R
+++ app.R
@@ -7,13 +7,6 @@ library(leaflet)
 # increase max R-Shiny user-input file size from 5 to 30 MB
 options(shiny.maxRequestSize = 30 * 1024 ^ 2)
 
-# the ui object has all the information for the user-interface
-ui <- fluidPage(
-  h1("Viral Space Time"),
-  theme = shinytheme("sandstone"),
-  leafletOutput("mymap")
-)
-
 # define fonts for plot
 f1 <- list(family = "Arial, sans-serif",
            size = 24,
@@ -36,13 +29,25 @@ m <- list(
   pad = 4
 )
 
+# the ui object has all the information for the user-interface
+ui <- fluidPage(
+  h1("Viral Space Time"),
+  theme = shinytheme("sandstone"),
+  leafletOutput("mymap")
+)
+
 
 server <- function(input, output, session) {
   output$mymap <- renderLeaflet({
     leaflet() %>%
-      leaflet() %>%
-      addTiles() %>%  # Add default OpenStreetMap map tiles
-      addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+      setView(lng=174.768, lat=-36.852, zoom=17 ) %>%
+      # Add two tiles
+      addProviderTiles("Esri.WorldImagery", group="Satellite Map") %>%
+      addTiles(options = providerTileOptions(noWrap = TRUE), group="Street Map") %>%
+      addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R") %>%
+      addCircleMarkers(lng=174.767, lat=-36.852, radius=19 , color="black",  fillColor="red", stroke = FALSE, fillOpacity = 0.8, group="Red") %>%
+      addCircleMarkers(lng=174.769, lat=-36.852, radius=18 , color="black",  fillColor="blue", stroke = FALSE, fillOpacity = 0.8, group="Blue") %>%
+      addLayersControl(overlayGroups = c("Red","Blue") , baseGroups = c("Satellite Map","Street Map"), options = layersControlOptions(collapsed = FALSE))
   })
 }
 
