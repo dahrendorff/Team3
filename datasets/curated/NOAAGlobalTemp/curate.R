23ffb693c97a4bca19038e4fb059b9c533afeaa6
jjmadsen <jesper.madsen@gmail.com>
2021-02-24 16:04:36 -0500

    Update curate.R

--- datasets/curated/NOAAGlobalTemp/curate.R
+++ datasets/curated/NOAAGlobalTemp/curate.R
@@ -1,6 +1,8 @@
 # Code is taken from, or inspired by:
-# November Temperature Anomaly" Shammun (URL: https://rpubs.com/KoushikChowdhury/329356)
-
+# November Temperature Anomaly" Shammun (URL: https://rpubs.com/KoushikChowdhury/329356), which in turn states that:
+# "This article to most extent is inspired by the article by Samuel S.P. Shen which you can download from this address: [Climate Data Analysis Using R] (httP;//shen.sdsu.edu/pdf/R-TextBySamShen2017.pdf)"
+#
+#
 library(RNetCDF)
 
 da1 = scan("NOAAGlobalTemp.gridded.v4.0.1.201711.asc")
