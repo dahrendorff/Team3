7cee128b0e96cf5b2b3efa4c8da6f0b6643ce741
Okey <dynamicsoorya2691@gmail.com>
2021-02-24 14:01:01 -0500

    First commit

---
+++ app.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(plotly)
+library(tidyverse)
+library(shinythemes)
+
+# increase max R-Shiny user-input file size from 5 to 30 MB
+options(shiny.maxRequestSize = 30 * 1024 ^ 2)
+
+# the ui object has all the information for the user-interface
+ui <- fluidPage(
+  h1("Viral SpaceTime"),
+  theme = shinytheme("sandstone"),
+)
+
+# define fonts for plot
+f1 <- list(family = "Arial, sans-serif",
+           size = 24,
+           color = "black")
+
+f2 <- list(family = "Arial, sans-serif",
+           size = 20,
+           color = "black")
+
+f3 <- list(family = "Arial, sans-serif",
+           size = 16,
+           color = "black")
+
+# set plot margins
+m <- list(
+  l = 20,
+  r = 20,
+  b = 10,
+  t = 100,
+  pad = 4
+)
+
+
+server <- function(input, output, session) {
+}
+
+
+shinyApp(ui, server)
